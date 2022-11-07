82a01f3300ca77187192af13d2ba1c59991ce098
lfpperru <lfpperru@hotmail.com>
2022-11-07 08:02:38 -0300

    organizing folders, gitignore and script

--- auto.sh
+++ auto.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+g++ src/main.cpp -o main
 exec=./main
-for i in {1..3}
+for i in {1..4}
 do 
   for k in {2..4} 
   do 
-    $exec instancias/instancia-$i.txt saidas/saida-$i-$k.txt $k 10 1.0 0.5; 
+    $exec input/dataset$i.txt output/output-$i-$k.txt $k 10 1.0 0.5; 
     # ./nome_do_programa nome_arquivo_entrada.txt nome_arquivo_saida.txt n_classes max_it chance_buscar chance_perturbar  
   done
 done
